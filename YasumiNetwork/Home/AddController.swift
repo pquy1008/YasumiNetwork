c212e5c27576e7a7979ccd05da69df269eedc866
huancaoz <huancaopro93@gmail.com>
2019-01-25 10:07:42 +0700

    create add screen

---
+++ YasumiNetwork/Home/AddController.swift
@@ -0,0 +1,9 @@
+//
+//  AddController.swift
+//  YasumiNetwork
+//
+//  Created by Huan CAO on 1/21/19.
+//  Copyright © 2019 Quy Pham. All rights reserved.
+//
+
+import Foundation
---
+++ YasumiNetwork/Home/AddViewController.swift
@@ -0,0 +1,76 @@
+//
+//  AddViewController.swift
+//  YasumiNetwork
+//
+//  Created by Huan CAO on 1/24/19.
+//  Copyright © 2019 Quy Pham. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddViewController: UIViewController {
+    
+    @IBOutlet weak var viewContainer: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    lazy var yasumiViewController: YasumiViewController = {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        var viewController = storyboard.instantiateViewController(withIdentifier: "YasumiViewController") as! YasumiViewController
+        
+        self.addViewControllerAsChildViewController(childViewController: viewController)
+        
+        return viewController
+    }()
+    
+    lazy var leaveViewController: LeaveViewController = {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        var viewController = storyboard.instantiateViewController(withIdentifier: "LeaveViewController") as! LeaveViewController
+        
+        self.addViewControllerAsChildViewController(childViewController: viewController)
+        
+        return viewController
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    func setupView() {
+        setupSegmentedControl()
+        
+        updateView()
+    }
+    
+    private func updateView() {
+        yasumiViewController.view.isHidden = !(segmentedControl.selectedSegmentIndex == 0)
+        leaveViewController.view.isHidden = !(segmentedControl.selectedSegmentIndex == 1)
+    }
+    
+    func setupSegmentedControl() {
+        segmentedControl.removeAllSegments()
+        segmentedControl.insertSegment(withTitle: "Yasumi", at: 0, animated: false)
+        segmentedControl.insertSegment(withTitle: "Leave", at: 1, animated: false)
+        segmentedControl.addTarget(self, action: #selector(selectionDidChange(sender:)), for: .valueChanged)
+        
+        segmentedControl.selectedSegmentIndex = 0
+    }
+    
+    @objc func selectionDidChange(sender: UISegmentedControl) {
+        updateView()
+    }
+    
+    private func addViewControllerAsChildViewController(childViewController: UIViewController) {
+        addChild(childViewController)
+        
+        self.viewContainer.addSubview(childViewController.view)
+        
+        childViewController.view.frame = view.bounds
+        childViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        childViewController.didMove(toParent: self)
+    }
+    
+}
