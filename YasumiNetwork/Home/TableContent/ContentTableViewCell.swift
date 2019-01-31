6ba3c242c912eb5dc290ea66e0e19d8bd7992607
huancaoz <huancaopro93@gmail.com>
2019-01-31 16:26:46 +0700

    add screen

---
+++ YasumiNetwork/Home/TableContent/ContentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContentTableViewCell.swift
+//  YasumiNetwork
+//
+//  Created by Huan CAO on 1/31/19.
+//  Copyright © 2019 Quy Pham. All rights reserved.
+//
+
+import UIKit
+
+class ContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
